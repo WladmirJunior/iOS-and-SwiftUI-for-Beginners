5321f37a274cd1dcea24fee1036160f7fa8809fd
Wladmir <wlad.w.junior@gmail.com>
2021-05-23 14:35:48 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Wladmir  on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
